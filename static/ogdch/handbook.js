55bdfc53b4ac5440760975743d7f89ddc27a2ecb
Oleg Lavrovsky <oleg@utou.ch>
2015-12-14 17:34:56 +0100

    Multilingual footer/nav

--- static/ogdch/handbook.js
+++ static/ogdch/handbook.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
     $('table').addClass('table table-striped table-hover');
+
+    var lang = $('html').attr('lang');
+    var $curlang = $('#lang-switch li[lang="' + lang + '"]').addClass('active');
+    $('#lang-button .name').html($curlang.text());
+
+    var DEFAULT_LANG = 'de';
+    if (lang !== DEFAULT_LANG) { $('.lang-' + DEFAULT_LANG).addClass('hidden'); }
+    $('.lang-' + lang).removeClass('hidden');
 });
 
 function validateForm(query) {
